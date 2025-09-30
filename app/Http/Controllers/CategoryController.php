2d4579250ee68a389c17fb37093508f85af235b2
quarentyne <peresada1997@gmail.com>
2025-09-30 18:57:13 +0000

    implement catefory controller and policy

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Category;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Gate;
+
+class CategoryController extends Controller
+{
+    public function index()
+    {
+        $categories = auth()->user()->categories;
+
+        dd($categories);
+    }
+
+    public function create()
+    {
+        //
+    }
+
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'name' => 'required|min:2|max:255',
+        ]);
+
+        Category::create([
+            'name' => $validated['name'],
+            'user_id' => auth()->id(),
+        ]);
+    }
+
+    public function show(Category $category)
+    {
+        Gate::authorize('view', $category);
+    }
+
+    public function edit(Category $category)
+    {
+        Gate::authorize('update', $category);
+    }
+
+    public function update(Request $request,Category $category)
+    {
+        Gate::authorize('update', $category);
+
+        $validated = $request->validate([
+            'name' => 'required|min:2|max:255',
+        ]);
+
+        $category->update($validated);
+    }
+
+    public function destroy(Category $category)
+    {
+        Gate::authorize('delete', $category);
+
+        $category->delete();
+    }
+}
--- app/Models/Category.php
+++ app/Models/Category.php
@@ -2,11 +2,16 @@
 
 namespace App\Models;
 
+use App\Policies\CategoryPolicy;
+use Illuminate\Database\Eloquent\Attributes\UsePolicy;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
+#[UsePolicy(CategoryPolicy::class)]
 class Category extends Model
 {
+    protected $guarded = [];
+
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
