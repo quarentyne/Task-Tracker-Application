54417f21aa694b357428f9b8e2812d0669dc6f3f
quarentyne <peresada1997@gmail.com>
2025-10-11 13:35:36 +0000

    create sidebar

--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -2,8 +2,8 @@
 
 @php
 $classes = ($active ?? false)
-            ? 'inline-flex items-center px-1 pt-1 border-b-2 border-indigo-400 text-sm font-medium leading-5 text-gray-900 focus:outline-none focus:border-indigo-700 transition duration-150 ease-in-out'
-            : 'inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out';
+            ? 'inline-flex items-center p-4 border-transparent rounded-xl font-medium text-red-400 bg-white transition duration-150 ease-in-out fill-red-400'
+            : 'inline-flex items-center p-4 border-transparent rounded-xl font-medium text-white hover:text-red-400 hover:fill-red-400 hover:bg-white transition duration-150 ease-in-out';
 @endphp
 
 <a {{ $attributes->merge(['class' => $classes]) }}>
